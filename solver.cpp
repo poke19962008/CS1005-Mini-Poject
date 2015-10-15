701066196afb3e2250acd7ede1ee559cce049a35
Sayan Das <poke19962008@gmail.com>
2015-10-15 19:19:44 +0530

    Initial Setup for BFS

--- solver.cpp
+++ solver.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 #include <array>
+#include <vector>
+#include <string>
 #include "rubik.h"
 
 using namespace std;
+
+vector<string> shortest_path(){
+
+}
+
 
 int main(){
 	rubik obj;
 
 	// TODO: @saurabh Test for all 6 moves on this set
 	array<int, 24> position = {6, 7, 8, 20, 18, 19, 3, 4, 5, 16, 17, 15, 0, 1, 2, 14, 12, 13, 10, 11, 9, 21, 22, 23};
-	array<int, 24> newPosition = obj.perm_apply("F", position);
+	array<int, 24> newPosition = obj.perm_apply("Fi", position);
 
 	for(int i=0;i<24;i++) cout<<newPosition[i]<<" ";
 	cout<<endl;
 
-	//Testing: get_move and get_move_name
-	array<int, 24> L_move = obj.get_move("L");
-	cout<<"Left Move: ";
-	for (int i = 0; i < 24; ++i) cout<<L_move[i];
-	cout<<endl;
-	cout<<"Left Move Name: "<<obj.get_move_name(L_move)<<endl;
+	
 
 	return 0;
 }
